ef2cc9e0b6714385f209c1e4e9aae02f67e1bbb2
Kalutu <kalutudaniel@gmail.com>
2024-06-13 02:44:23 +0300

    Db connection

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 
-import xxs from "xxs";
-import mongoSanitize from "mongo-sanitize";
+import xxs from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+import dbConnection from "./dbConfig/dbConfig.js";
 
 dotenv.config();
 
@@ -14,3 +15,18 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 //MONGODB CONNECTION
+dbConnection();
+
+//middleware
+app.use(cors());
+app.use(xxs());
+app.use(bodyParser.json());
+app.use(mongoSanitize());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
